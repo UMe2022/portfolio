86de03b8284b51ddd7a8baf57eae9951696d1061
U ME <umidartikv92@gmail.com>
2022-08-24 04:10:31 +0500

    fully completed first display

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,11 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
 
+hamburger.addEventListener('click', () => {
+    menu.classList.add('menu_active');
+});
 
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('menu_active');
+});

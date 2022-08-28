4ddf43b261671a6827edfa98a9bef57ac885e220
U ME <umidartikv92@gmail.com>
2022-08-28 19:30:00 +0500

    deploy

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
+      menuOverlay = document.querySelector('.menu__overlay')
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('menu_active');
 });
 
 closeElem.addEventListener('click', () => {
+    menu.classList.remove('menu_active');
+});
+
+menuOverlay.addEventListener('click', () => {
     menu.classList.remove('menu_active');
 });
